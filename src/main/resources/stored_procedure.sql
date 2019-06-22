16fb593429b284a46cea72ff7568a7f114634483
Willy Sanchez Rojas <willy.sanchez@dharbor.com>
2019-06-22 12:11:21 -0400

    add procedures in sql in resources

--- src/main/resources/stored_procedure.sql
+++ src/main/resources/stored_procedure.sql
@@ -15,10 +15,70 @@
 --GO
 --SET QUOTED_IDENTIFIER ON
 --GO
-CREATE PROCEDURE [dbo].[ALL_CATEGORIA]
-AS
+
+
+CREATE procedure GET_ALL_CATEGORIA
+as
+SET XACT_ABORT ON;
+SET NOCOUNT ON;
 BEGIN
-   SELECT *
-   FROM Categoria
-END;
+	select *
+	from Categoria
+END
+GO
+
+
+CREATE procedure GET_CATEGORIA_BY_ID
+(
+	@idCategoria int
+)
+as
+SET XACT_ABORT ON;
+SET NOCOUNT ON;
+BEGIN
+	select *
+	from dbo.Categoria	categoria
+	where categoria.id = @idCategoria;
+END
+GO
+
+
+CREATE procedure DELETE_CATEGORIA
+(
+	@idCategoria int
+)
+as
+SET XACT_ABORT ON;
+SET NOCOUNT ON;
+BEGIN
+	delete from Categoria where id = @idCategoria;
+END
+GO
+
+create procedure DELETE_CATEGORIA
+(
+	@idCategoria int
+)
+as
+SET XACT_ABORT ON;
+SET NOCOUNT ON;
+BEGIN
+	select * from Categoria where id = @idCategoria;
+	delete from Categoria where id = @idCategoria;
+END
+GO
+
+create procedure INSERT_CATEGORIA
+(
+	@descripcion varchar(50)
+)
+as
+SET XACT_ABORT ON;
+SET NOCOUNT ON;
+BEGIN
+	insert into Categoria(descripcion)values(@descripcion);
+	select * from Categoria where id=@@IDENTITY;
+END
+GO
+
 
